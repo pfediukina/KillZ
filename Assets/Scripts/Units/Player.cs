9f8e4ce7285f8035c358e32606f5084ab09474ae
Polina Fediukina <61268493+pfediukina@users.noreply.github.com>
2023-02-25 12:40:45 +0200

    move fix

--- Assets/Scripts/Units/Player.cs
+++ Assets/Scripts/Units/Player.cs
@@ -16,11 +16,7 @@ public class Player : Unit
     private void Move()
     {
         GetInput(out NetworkInputData data);
-
         if (data.Direction.x != 0) _spriteRenderer.flipX = data.Direction.x > 0 ? false : true;
-        //Vector3 move = dir;
-        //move.x = dir.x;
-        //move.y = dir.y;
-        transform.Translate(data.Direction * _info.StartSpeed * Runner.DeltaTime); // position += dir * _info.StartSpeed * Runner.DeltaTime;
+        transform.Translate(data.Direction * _info.StartSpeed * Runner.DeltaTime);
     }
 }
